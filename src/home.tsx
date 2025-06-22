c0eae861ddc6dec61e8efe5356d0f3c0174f7045
Greg-prog <olajideemmanuel250@gmail.com>
2025-06-22 23:47:35 +0100

    about us

--- src/home.tsx
+++ src/home.tsx
@@ -1,11 +1,13 @@
 import NavigationBar from "./components/navigation";
 import Hero from "./components/Hero";
 import Products from "./components/products";
+import AboutUsSection from "./components/aboutUs";
 function Home(){
     return <div className="w-[100vw] px-10 bg-gradient-to-b from-[#C8EC9] to-green-400">
         <NavigationBar/>
         <Hero/>
         <Products/>
+        <AboutUsSection/>
     </div>
 }
 export default Home
