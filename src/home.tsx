02938be6c60381968cd30d1645ea71bb61ef2829
Greg-prog <olajideemmanuel250@gmail.com>
2025-07-25 14:32:52 +0100

    white space

--- src/home.tsx
+++ src/home.tsx
@@ -9,7 +9,7 @@ import Plans from "./components/plans";
 import Founder from "./components/founder";
 import Projects from "./components/projects";
 function Home(){
-    return <div className="w-[100vw] px-[4px]  bg-gradient-to-b from-[#daf3da] to-[white]" style={{fontFamily: "Roboto Slab"}}>
+    return <div className="w-[100vw] px-[4px]  bg-gradient-to-b from-[#daf3da] overflow-hidden to-[white]" style={{fontFamily: "Roboto Slab"}}>
         <NavigationBar/>
         <Hero/>
         <Products/>
