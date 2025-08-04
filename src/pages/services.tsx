99c8cde60fcbe0034c910999848c4eadb6ceb099
Greg-prog <olajideemmanuel250@gmail.com>
2025-08-04 21:03:07 +0100

    feat:fxed build error

--- src/pages/services.tsx
+++ src/pages/services.tsx
@@ -1,12 +1,20 @@
 // import NavigationBar from "./"
 import NavigationBar from "../components/navigation"
 
-function Service(){
+function Plans(){
    
     return <>
-        <div>
-
+        <div className="w-[100vw] px-[4px]  bg-gradient-to-b from-[#daf3da] overflow-hidden to-[white]" style={{fontFamily: "Roboto Slab"}} >
+            <NavigationBar/>
+            <div>
+                <p>These are the plans Optimum solar provides</p>
+            </div>
+            <div>
+                    
+            </div>
         </div>
     </>
 }
 
+
+export default Plans
