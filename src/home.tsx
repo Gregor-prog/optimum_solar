09d6a8a4737381e839634935a9891d1db97c42e9
Greg-prog <olajideemmanuel250@gmail.com>
2025-08-05 20:29:35 +0100

    chanegd whatsapp widget

--- src/home.tsx
+++ src/home.tsx
@@ -9,18 +9,21 @@ import Plans from "./components/plans";
 import Founder from "./components/founder";
 import Projects from "./components/projects";
 import { FloatingWhatsApp } from 'react-floating-whatsapp'
+import OPt from "./assets/IMG-20250620-WA0212_1__2-removebg-preview.png"
 function Home(){
     return <div className="w-[100vw] px-[4px]  bg-gradient-to-b from-[#daf3da] overflow-hidden to-[white]" style={{fontFamily: "Roboto Slab"}}>
         <FloatingWhatsApp
             phoneNumber="+2347036273661"
             accountName="optimum solar gadget"
             statusMessage="typically replies in 10mins"
-            chatMessage="Hello there!\nHow can we help?"
+            chatMessage="Hello there! How can we help?"
             placeholder="type a messsage..."
             allowClickAway
             buttonClassName="whatsapp icon"
             chatboxHeight={320}
             notification
+            darkMode
+            avatar={OPt}
         />
         <NavigationBar/>
         <Hero/>
